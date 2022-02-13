0f8d677f2c24bd0f91f2674265e0defe13d5477c
Yamil-Layicov <yamilbl@code.edu.az>
2022-02-13 19:07:50 +0400

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VusalTaskBook
+{
+    class Book
+    {
+        private string _bookName;
+        private int _pageCount;
+
+        public Book(string name , int page)
+        {
+            BookName = name;
+            PageCount = page;
+        }
+
+        public string BookName
+        {
+            get { return _bookName; }
+            set
+            {
+                while (true)
+                {
+                    if (value.Length > 3)
+                    {
+                        _bookName = value;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Kitabin adini duzugun daxil edin");
+                        value = Console.ReadLine();
+                    }
+                }
+            }
+        }
+
+        public int PageCount
+        {
+            get { return _pageCount; }
+            set
+            {
+                while (true)
+                {
+                    if (value > 10)
+                    {
+                        _pageCount = value;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Kitab sehifesini duzgun daxil edin");
+                        int.TryParse(Console.ReadLine(), out value);
+                    }
+                }
+            }
+        } 
+
+        public string Info()
+        {
+            return $"{_bookName} kitabi {_pageCount} sehifeden ibaretdir.";
+        }
+      
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VusalTaskBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] arr = new Book[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"{i + 1} kitabinin adi: ");
+                string book_name = Console.ReadLine();
+                Console.WriteLine($"{i + 1} kitabinin sahifesin daxil et: ");
+                int book_page;
+                int.TryParse(Console.ReadLine(), out book_page);
+                Book book = new Book(book_name, book_page);
+                arr[i] = book;
+
+            }
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item.Info());
+            }
+        }
+    }
+}
